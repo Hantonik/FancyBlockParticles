1841028d89448de57f6763ad01fcd9a3bed45492
Hantonik <piotrmatusiewicz85@gmail.com>
2024-02-18 23:27:31 +0100

    Fix crash with SereneSeasons mod (#1)

---
+++ Forge/src/main/java/hantonik/fbp/util/FBPUtils.java
@@ -0,0 +1,16 @@
+package hantonik.fbp.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraftforge.fml.ModList;
+import sereneseasons.season.SeasonHooks;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class FBPUtils {
+    public static Biome.Precipitation getPrecipitationAtLevelRenderer(Holder<Biome> biome, BlockPos pos) {
+        return ModList.get().isLoaded("sereneseasons") ? SeasonHooks.getPrecipitationAtLevelRendererHook(biome, pos) : biome.value().getPrecipitationAt(pos);
+    }
+}
