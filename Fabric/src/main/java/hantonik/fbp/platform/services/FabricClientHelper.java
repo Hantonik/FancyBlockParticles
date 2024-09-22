1a32783c5ffc3119cb3b6b5ec620e8affa3a004b
Hantonik <piotrmatusiewicz85@gmail.com>
2024-09-23 00:15:45 +0200

    Fix shaders incompatibility, fixes #10, fixes #24

--- Fabric/src/main/java/hantonik/fbp/platform/services/FabricClientHelper.java
+++ Fabric/src/main/java/hantonik/fbp/platform/services/FabricClientHelper.java
@@ -2,10 +2,16 @@ package hantonik.fbp.platform.services;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import hantonik.fbp.FancyBlockParticles;
+import net.coderbot.iris.Iris;
+import net.coderbot.iris.pipeline.ShaderAccess;
+import net.coderbot.iris.pipeline.newshader.CoreWorldRenderingPipeline;
+import net.coderbot.iris.pipeline.newshader.ShaderKey;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.ShaderInstance;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
@@ -19,5 +25,26 @@ public final class FabricClientHelper implements IClientHelper {
         var renderer = Minecraft.getInstance().getBlockRenderer();
 
         renderer.getModelRenderer().tesselateBlock(level, renderer.getBlockModel(state), state, pos, stack, bufferSource.getBuffer(ItemBlockRenderTypes.getRenderType(state, FancyBlockParticles.CONFIG.global.isCullParticles())), false, new Random(), state.getSeed(pos), OverlayTexture.NO_OVERLAY);
+    }
+
+    @Override
+    public ShaderInstance getParticleTranslucentShader() {
+        return FabricLoader.getInstance().isModLoaded("iris") ? ShaderAccess.getParticleTranslucentShader() : IClientHelper.super.getParticleTranslucentShader();
+    }
+
+    @Override
+    public ShaderInstance getBlockTranslucentShader() {
+        if (FabricLoader.getInstance().isModLoaded("iris")) {
+            var pipeline = Iris.getPipelineManager().getPipelineNullable();
+
+            if (pipeline instanceof CoreWorldRenderingPipeline shaderPipeline) {
+                var shader = shaderPipeline.getShaderMap().getShader(ShaderKey.MOVING_BLOCK);
+
+                if (shader != null)
+                    return shader;
+            }
+        }
+
+        return IClientHelper.super.getBlockTranslucentShader();
     }
 }
--- Forge/src/main/java/hantonik/fbp/platform/services/ForgeClientHelper.java
+++ Forge/src/main/java/hantonik/fbp/platform/services/ForgeClientHelper.java
@@ -1,10 +1,14 @@
 package hantonik.fbp.platform.services;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.coderbot.iris.Iris;
+import net.coderbot.iris.pipeline.newshader.CoreWorldRenderingPipeline;
+import net.coderbot.iris.pipeline.newshader.ShaderKey;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.ShaderInstance;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
@@ -25,6 +29,38 @@ public final class ForgeClientHelper implements IClientHelper {
         var buffer = bufferSource.getBuffer(type);
 
         Minecraft.getInstance().getBlockRenderer().getModelRenderer().tesselateBlock(level, model, state, pos, stack, buffer, false, new Random(), state.getSeed(pos), OverlayTexture.NO_OVERLAY, EmptyModelData.INSTANCE);
+    }
+
+    @Override
+    public ShaderInstance getParticleTranslucentShader() {
+        if (ModList.get().isLoaded("oculus")) {
+            var pipeline = Iris.getPipelineManager().getPipelineNullable();
+
+            if (pipeline instanceof CoreWorldRenderingPipeline shaderPipeline) {
+                var shader = shaderPipeline.getShaderMap().getShader(ShaderKey.PARTICLES);
+
+                if (shader != null)
+                    return shader;
+            }
+        }
+
+        return IClientHelper.super.getParticleTranslucentShader();
+    }
+
+    @Override
+    public ShaderInstance getBlockTranslucentShader() {
+        if (ModList.get().isLoaded("oculus")) {
+            var pipeline = Iris.getPipelineManager().getPipelineNullable();
+
+            if (pipeline instanceof CoreWorldRenderingPipeline shaderPipeline) {
+                var shader = shaderPipeline.getShaderMap().getShader(ShaderKey.MOVING_BLOCK);
+
+                if (shader != null)
+                    return shader;
+            }
+        }
+
+        return IClientHelper.super.getBlockTranslucentShader();
     }
 
     @Override
