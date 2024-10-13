a047f5b43295025c6767a8fa3ed4e9b55bbfde26
Hantonik <piotrmatusiewicz85@gmail.com>
2024-10-13 21:56:46 +0200

    Fix Done button disappearing from screen, fixes #17

--- Common/src/main/java/hantonik/fbp/mixin/MixinOptionsScreen.java
+++ Common/src/main/java/hantonik/fbp/mixin/MixinOptionsScreen.java
@@ -3,6 +3,7 @@ package hantonik.fbp.mixin;
 import hantonik.fbp.screen.FBPOptionsScreen;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.layouts.GridLayout;
+import net.minecraft.client.gui.layouts.SpacerElement;
 import net.minecraft.client.gui.screens.OptionsScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
@@ -10,6 +11,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -19,6 +21,11 @@ import java.util.function.Supplier;
 public abstract class MixinOptionsScreen {
     @Shadow
     protected abstract Button openScreenButton(Component title, Supplier<Screen> screen);
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/layouts/SpacerElement;height(I)Lnet/minecraft/client/gui/layouts/SpacerElement;", ordinal = 0), method = "init")
+    protected SpacerElement height(int height) {
+        return SpacerElement.height(height - 15);
+    }
 
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/Button;builder(Lnet/minecraft/network/chat/Component;Lnet/minecraft/client/gui/components/Button$OnPress;)Lnet/minecraft/client/gui/components/Button$Builder;"), locals = LocalCapture.CAPTURE_FAILHARD, method = "init")
     protected void init(CallbackInfo callback, GridLayout layout, GridLayout.RowHelper helper) {
