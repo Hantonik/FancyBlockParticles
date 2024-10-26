6b20059b004f5add19d14eb0e5d3876bb6bba97c
Hantonik <piotrmatusiewicz85@gmail.com>
2024-10-26 23:19:53 +0200

    Code cleanup

--- Common/src/main/java/hantonik/fbp/mixin/MixinOptionsScreen.java
+++ Common/src/main/java/hantonik/fbp/mixin/MixinOptionsScreen.java
@@ -1,9 +1,9 @@
 package hantonik.fbp.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import hantonik.fbp.screen.FBPOptionsScreen;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.layouts.GridLayout;
-import net.minecraft.client.gui.layouts.LinearLayout;
 import net.minecraft.client.gui.screens.OptionsScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
@@ -12,7 +12,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.function.Supplier;
 
@@ -21,8 +20,8 @@ public abstract class MixinOptionsScreen {
     @Shadow
     protected abstract Button openScreenButton(Component title, Supplier<Screen> screen);
 
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/layouts/HeaderAndFooterLayout;addToContents(Lnet/minecraft/client/gui/layouts/LayoutElement;)Lnet/minecraft/client/gui/layouts/LayoutElement;"), locals = LocalCapture.CAPTURE_FAILHARD, method = "init")
-    protected void init(CallbackInfo callback, LinearLayout verticalLayout, LinearLayout horizontalLayout, GridLayout grid, GridLayout.RowHelper helper) {
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/layouts/HeaderAndFooterLayout;addToContents(Lnet/minecraft/client/gui/layouts/LayoutElement;)Lnet/minecraft/client/gui/layouts/LayoutElement;"), method = "init")
+    protected void init(CallbackInfo callback, @Local GridLayout.RowHelper helper) {
         helper.addChild(this.openScreenButton(Component.translatable("key.fbp.category").append("..."), () -> new FBPOptionsScreen((OptionsScreen) (Object) this)));
     }
 }
