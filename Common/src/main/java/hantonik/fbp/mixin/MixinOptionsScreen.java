fe4b25d458ba3f34f6949e1cb093c70ec9c3051f
Hantonik <piotrmatusiewicz85@gmail.com>
2024-10-26 23:19:53 +0200

    Code cleanup

--- Common/src/main/java/hantonik/fbp/mixin/MixinOptionsScreen.java
+++ Common/src/main/java/hantonik/fbp/mixin/MixinOptionsScreen.java
@@ -1,5 +1,6 @@
 package hantonik.fbp.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import hantonik.fbp.screen.FBPOptionsScreen;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.layouts.GridLayout;
@@ -13,7 +14,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.function.Supplier;
 
@@ -27,8 +27,8 @@ public abstract class MixinOptionsScreen {
         return SpacerElement.height(height - 15);
     }
 
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/Button;builder(Lnet/minecraft/network/chat/Component;Lnet/minecraft/client/gui/components/Button$OnPress;)Lnet/minecraft/client/gui/components/Button$Builder;"), locals = LocalCapture.CAPTURE_FAILHARD, method = "init")
-    protected void init(CallbackInfo callback, GridLayout layout, GridLayout.RowHelper helper) {
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/Button;builder(Lnet/minecraft/network/chat/Component;Lnet/minecraft/client/gui/components/Button$OnPress;)Lnet/minecraft/client/gui/components/Button$Builder;"), method = "init")
+    protected void init(CallbackInfo callback, @Local GridLayout.RowHelper helper) {
         helper.addChild(this.openScreenButton(Component.translatable("key.fbp.category").append("..."), () -> new FBPOptionsScreen((OptionsScreen) (Object) this)));
     }
 }
