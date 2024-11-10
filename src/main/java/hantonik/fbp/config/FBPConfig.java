2bb2b9e44deed8456c5d320ddc838348c41596f1
Hantonik <piotrmatusiewicz85@gmail.com>
2024-11-11 00:34:32 +0100

    Fix rain and snow particle density option not saving, closes #40

--- src/main/java/hantonik/fbp/config/FBPConfig.java
+++ src/main/java/hantonik/fbp/config/FBPConfig.java
@@ -925,7 +925,7 @@ public final class FBPConfig implements IFBPConfig<FBPConfig> {
             json.addProperty("simulationDistance", this.simulationDistance);
 
             json.addProperty("transparency", this.transparency);
-            json.addProperty("rainParticleDensity", this.particleDensity);
+            json.addProperty("particleDensity", this.particleDensity);
 
             json.addProperty("sizeMultiplier", this.sizeMultiplier);
             json.addProperty("gravityMultiplier", this.gravityMultiplier);
@@ -1094,7 +1094,7 @@ public final class FBPConfig implements IFBPConfig<FBPConfig> {
             json.addProperty("renderDistance", this.renderDistance);
             json.addProperty("simulationDistance", this.simulationDistance);
 
-            json.addProperty("snowParticleDensity", this.particleDensity);
+            json.addProperty("particleDensity", this.particleDensity);
 
             json.addProperty("sizeMultiplier", this.sizeMultiplier);
             json.addProperty("rotationMultiplier", this.rotationMultiplier);
