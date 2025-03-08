a843f143f7b0667eb69591ae83972b06c4774127
Hantonik <piotrmatusiewicz85@gmail.com>
2025-03-08 17:45:17 +0100

    Compatible with Iris, fixes #38

---
+++ Fabric/src/main/java/hantonik/fbp/mixin/MixinBlockRenderInfo.java
@@ -0,0 +1,29 @@
+package hantonik.fbp.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import hantonik.fbp.animation.FBPPlacingAnimationManager;
+import net.fabricmc.fabric.impl.client.indigo.renderer.render.BlockRenderInfo;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(BlockRenderInfo.class)
+public abstract class MixinBlockRenderInfo {
+    @Final
+    @Shadow
+    private BlockPos.MutableBlockPos searchPos;
+
+    @WrapOperation(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;shouldRenderFace(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/Direction;Lnet/minecraft/core/BlockPos;)Z"), method = "shouldDrawFace")
+    private boolean shouldDrawFace(BlockState state, BlockGetter level, BlockPos pos, Direction face, BlockPos facePos, Operation<Boolean> original) {
+        if (FBPPlacingAnimationManager.isHidden(this.searchPos))
+            return true;
+
+        return original.call(state, level, pos, face, facePos);
+    }
+}
