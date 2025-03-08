75fbca33cef2cc8e066e310c445431113eb8ca0e
Hantonik <piotrmatusiewicz85@gmail.com>
2025-03-08 17:45:17 +0100

    Compatible with Iris, fixes #38

--- Fabric/src/main/java/hantonik/fbp/mixin/sodium/MixinBlockOcclusionCache.java
+++ Fabric/src/main/java/hantonik/fbp/mixin/sodium/MixinBlockOcclusionCache.java
@@ -1,15 +1,14 @@
 package hantonik.fbp.mixin.sodium;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import hantonik.fbp.animation.FBPPlacingAnimationManager;
 import net.caffeinemc.mods.sodium.client.render.chunk.compile.pipeline.BlockOcclusionCache;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockState;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -17,13 +16,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Pseudo
 @Mixin(value = BlockOcclusionCache.class, remap = false)
 public abstract class MixinBlockOcclusionCache {
-    @Final
-    @Shadow
-    private BlockPos.MutableBlockPos cachedPositionObject;
-
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos$MutableBlockPos;set(III)Lnet/minecraft/core/BlockPos$MutableBlockPos;", shift = At.Shift.AFTER), method = "shouldDrawSide", cancellable = true)
-    public void shouldDrawSide(BlockState selfState, BlockGetter view, BlockPos selfPos, Direction facing, CallbackInfoReturnable<Boolean> callback) {
-        if (FBPPlacingAnimationManager.isHidden(this.cachedPositionObject))
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos$MutableBlockPos;set(III)Lnet/minecraft/core/BlockPos$MutableBlockPos;", shift = At.Shift.AFTER, remap = true), method = "shouldDrawSide", cancellable = true)
+    public void shouldDrawSide(BlockState selfState, BlockGetter view, BlockPos selfPos, Direction facing, CallbackInfoReturnable<Boolean> callback, @Local BlockPos.MutableBlockPos otherPos) {
+        if (FBPPlacingAnimationManager.isHidden(otherPos))
             callback.setReturnValue(true);
     }
 }
