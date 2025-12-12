d1e1ef598dd3b8c5559ed401831bcf0bb7212347
Hantonik <piotrmatusiewicz85@gmail.com>
2025-12-12 01:41:56 +0100

    Fix seeing through the world when block placed without indigo renderer

--- Fabric/src/main/java/hantonik/fbp/mixin/MixinModelBlockRenderer.java
+++ Fabric/src/main/java/hantonik/fbp/mixin/MixinModelBlockRenderer.java
@@ -16,7 +16,7 @@ public abstract class MixinModelBlockRenderer {
     @WrapOperation(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;shouldRenderFace(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/Direction;)Z"), method = "shouldRenderFace")
     private static boolean shouldRenderFace(BlockState state, BlockState faceState, Direction face, Operation<Boolean> original, @Local(argsOnly = true) BlockPos pos) {
         if (FBPPlacingAnimationManager.isHidden(pos))
-            return false;
+            return true;
 
         return original.call(state, faceState, face);
     }
